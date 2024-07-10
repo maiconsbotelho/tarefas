c45d49b3da339a4467ae738c03283ec6586d7b8b
Maicon Botelho <maicon.sbotelho@hotmail.com>
2024-07-10 20:47:18 -0300

    find

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/tarefas',
-  assetPrefix: '/tarefas/',
-  output: 'export', // Add this line
 };
 
 export default nextConfig;
